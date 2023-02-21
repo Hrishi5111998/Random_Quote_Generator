cb5125832c05efaaf8834370f4846a6a3b7be114
Hrishikesh Tule <hrishikeshtule1998@gmail.com>
2023-02-21 20:14:58 +0530

    git commit -m "initial changes"

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ let quote = document.querySelector('.quote')
 
  const quotes = [{
     quote: "The way to get started is to quit talking and begin doing.",
-    person: "Walt Disney" 
+    person: "-Walt Disney" 
  },
 {
     quote: "If life were predictable it would cease to be life, and be without flavor.",
-    person: "Eleanor Roosevelt"
+    person: "-Eleanor Roosevelt"
 },
 {
     quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
-    person: "Oprah Winfrey"
+    person: "-Oprah Winfrey"
 },
 {
     quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
-    person: "James Cameron"
+    person: "-James Cameron"
 },
 {
     quote: "Life is what happens when you're busy making other plans.",
-    person: "John Lennon"
+    person: "-John Lennon"
 },
 {
 quote: "You will face many defeats in life, but never let yourself be defeated.",
